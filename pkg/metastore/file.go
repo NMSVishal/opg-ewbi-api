8954d542e04291a070e585858128315815ab2dd3
NMSVishal <vishal-vasantrao.varvate@capgemini.com>
2025-11-11 11:49:50 +0000

    added

--- pkg/metastore/file.go
+++ pkg/metastore/file.go
@@ -6,9 +6,9 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/nbycomp/neonephos-opg-ewbi-api/api/federation/models"
-	camara "github.com/nbycomp/neonephos-opg-ewbi-api/api/federation/server"
-	opgv1beta1 "github.com/nbycomp/neonephos-opg-ewbi-operator/api/v1beta1"
+	"github.com/neonephos-katalis/opg-ewbi-api/api/federation/models"
+	camara "github.com/neonephos-katalis/opg-ewbi-api/api/federation/server"
+	opgv1beta1 "github.com/neonephos-katalis/opg-ewbi-operator/api/v1beta1"
 )
 
 type File struct {
